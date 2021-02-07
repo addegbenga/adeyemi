74559b7b9de73270da4d7d3862c0ccdffadb36aa
addegbenga <addegbenga@gmail.com>
2021-02-07 23:22:16 +0100

    fix

--- src/component/loading/Loading.js
+++ src/component/loading/Loading.js
@@ -1,13 +1,14 @@
 import React from "react";
-import gif from "../img/giphy.gif"
+// import gif from "../img/giphy.gif"
 
 export default function Loading() {
   return (
     <div className="loading-container">
-      <img
+      {/* <img
         src={gif}
         alt="loading"
-      ></img>
+      ></img> */}
+      ⏰⏰⏰⏰⏰⏰⏰⏰⏰⏰
     </div>
   );
 }
